cd22f5f678c6d7aad8ee53c5c2e3fa9c358a3b54
Kl0099 <lodhakrunal464@gmail.com>
2024-02-10 01:37:17 +0530

    add varifySignation function

--- server/controllers/Payments.js
+++ server/controllers/Payments.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const instance = require("../config/razorepay");
 const Course = require("../models/Course");
 const User = require("../models/User");
+const mailSender = require("../utils/mailSender");
 
 exports.capturePayment = async (req, res) => {
   const { courseId } = req.body;
@@ -42,6 +43,7 @@ exports.capturePayment = async (req, res) => {
     amount,
     currency,
     receipt: Math.random(Date.now()).toString(),
+    //why?  because in the future we need this notes after varification payments
     notes: {
       courseId: course.id,
       userId,
@@ -66,6 +68,70 @@ exports.capturePayment = async (req, res) => {
     return res.status(404).json({
       success: false,
       message: error.message,
+    });
+  }
+};
+
+exports.varifySignature = async (req, res) => {
+  const webHookSecrete = "12334234";
+
+  const signature = req.headers["X-razorepay-signature"];
+  const shasum = crypto.createHmac("sha256", webHookSecrete);
+  shasum.update(JSON.stringify(req.body));
+  const digest = shasum.digest("hex");
+  if (signature === digest) {
+    console.log("payment is authorized");
+    //take courseid and userid from razorepay notes
+    //req.body.payload.entity.notes
+    //after payment userschema ==> courses add courseid and
+    //courseschema => enrolledstudent => add userid
+    const { courseId, userId } = req.body.payload.entity.notes;
+    if (!courseId || !userId) {
+      return res.status(400).json({
+        success: false,
+        message: "razorepay server erorr",
+      });
+    }
+
+    try {
+      const enrolledcourse = await Course.findByIdAndUpdate(
+        { courseId },
+        { $push: { studentsEnrolled: userId } },
+        { new: true }
+      );
+      if (!enrolledcourse) {
+        return res.status(404).json({
+          success: false,
+          message: "course not found error in payment",
+        });
+      }
+      console.log(enrolledcourse);
+
+      const enrolledStudent = await User.findByIdAndUpdate(
+        { userId },
+        { $push: { courses: courseId } },
+        { new: true }
+      );
+      const emailRes = await mailSender(
+        enrolledStudent.email,
+        "congracts form code help",
+        "congrats "
+      );
+      res.status(200).json({
+        success: true,
+        message: "Signature verification completed successfully",
+      });
+    } catch (error) {
+      console.log("error occuresd while verifying signature", error);
+      res.status(500).json({
+        success: false,
+        message: "Signature verification failed",
+      });
+    }
+  } else {
+    return res.status(500).json({
+      success: false,
+      message: "invalid signature",
     });
   }
 };
