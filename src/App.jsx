a6c28704301f6c8d85363289e9363f14b1008553
Kl0099 <lodhakrunal464@gmail.com>
2024-03-08 18:29:30 +0530

    login page

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,15 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/common/Navbar";
 import Footer from "./components/common/Footer";
+import Login from "./pages/Login";
+import AboutUs from "./pages/AboutUs";
+import Register from "./pages/Register";
+import ContactUs from "./pages/ContactUs";
+import { useLocation } from "react-router-dom";
 
 function App() {
+  const location = useLocation();
+  console.log(location.pathname);
   return (
     <main className="flex min-h-screen w-screen flex-col bg-richblack-900 font-inter">
       <Navbar />
@@ -13,8 +20,26 @@ function App() {
           path="/"
           element={<Home />}
         />
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+        <Route
+          path="/register"
+          element={<Register />}
+        />
+        <Route
+          path="/aboutus"
+          element={<AboutUs />}
+        />
+        <Route
+          path="/contactus"
+          element={<ContactUs />}
+        />
       </Routes>
-      <Footer />
+      {location.pathname !== "/register" && location.pathname !== "/login" && (
+        <Footer />
+      )}
     </main>
   );
 }
---
+++ src/pages/Login.jsx
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+import CTAButton from "../components/core/homepage/Button";
+import { FaRegEye as OpenEye } from "react-icons/fa";
+import { FaRegEyeSlash as CloseEye } from "react-icons/fa";
+import Highlighted from "../components/core/homepage/Highlighted";
+const Login = () => {
+  const accounts = ["Student", "Instructors"];
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [visiblepassword, setVisiblePassword] = useState(false);
+  const [accountType, setAccountType] = useState("Student");
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+    // setVisiblePassword(e.target.value);
+  };
+  return (
+    <div className=" flex flex-col items-center justify-center h-[90vh]  ">
+      <div className="shadow-richblack-500  shadow-sm   flex  flex-col gap-8 text-white p-4 sm:w-[60%] lg:w-[30%]  md:w-[40%] ">
+        <div className="flex flex-col">
+          <h1 className=" text-3xl mb-3">Welcome Back</h1>
+          <p className=" text-sm text-richblack-500">Discover your passion</p>
+          <p className=" text-xs font-edu-sa">
+            <Highlighted text={" Be unstoppable"} />
+          </p>
+        </div>
+        <div className=" ml-1  flex gap-3 bg-richblue-800 w-fit rounded-full shadow-richblack-500  shadow-sm">
+          {accounts.map((item, index) => (
+            <div
+              key={index}
+              className={`${
+                accountType === item
+                  ? " bg-richblack-900 text-richblack-5"
+                  : " text-richblack-200"
+              } px-3 py-1 rounded-full cursor-pointer hover:scale-95 `}
+              onClick={() => setAccountType(item)}
+            >
+              {item}
+            </div>
+          ))}
+        </div>
+        <div className="flex flex-col items-center">
+          <form className="  w-full ">
+            <div className="flex flex-col items-start gap-2">
+              <label
+                htmlFor="email"
+                id="email"
+                className=" text-sm"
+              >
+                Email Address
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Enter email address"
+                className=" bg-richblack-800 p-2 rounded-md w-full"
+              />
+            </div>
+            <div className="relative flex flex-col items-start mt-5 gap-2">
+              <label
+                htmlFor="password"
+                id="password"
+                className=" text-sm "
+              >
+                Password
+              </label>
+              <div className=" w-full">
+                <input
+                  type={visiblepassword ? "text" : "password"}
+                  name="password"
+                  id="password"
+                  placeholder="Enter Password"
+                  className=" bg-richblack-800 p-2 rounded-md w-full"
+                  value={password}
+                  onChange={handlePasswordChange}
+                ></input>
+                <div
+                  className=""
+                  onClick={() => setVisiblePassword(!visiblepassword)}
+                >
+                  {visiblepassword ? (
+                    <CloseEye className=" right-2 -mt-[2rem] text-2xl absolute text-richblack-300" />
+                  ) : (
+                    <OpenEye className=" right-2 -mt-[2rem] text-2xl absolute text-richblack-300" />
+                  )}
+                </div>
+              </div>
+            </div>
+            <p className=" cursor-pointe  text-right text-sm text-richblue-300 mt-2">
+              forgot password
+            </p>
+            <div className=" w-full mt-8">
+              <CTAButton active={true}>Sign in</CTAButton>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
